0b6a164c55f1553e2a2449ba39aeccbe28826cbb
Jefferson Frade <jefferrson.frade@gmail.com>
2019-07-19 22:41:15 -0300

    Finalizacao do login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="usuario" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="usuario" class="col-md-4 col-form-label text-md-right">Usuário</label>
 
                             <div class="col-md-6">
                                 <input id="usuario" type="text" class="form-control @error('usuario') is-invalid @enderror" name="usuario" value="{{ old('usuario') }}" required autocomplete="usuario" autofocus>
